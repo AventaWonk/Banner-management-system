4bb3fd0de35f182b95d446f7c02696d44d105c73
Roman <aventawonk@gmail.com>
2018-09-09 23:43:02 +0300

    Update mappers

--- backend/src/main/java/com/splat/bms/config/Mapping.java
+++ backend/src/main/java/com/splat/bms/config/Mapping.java
@@ -2,6 +2,7 @@ package com.splat.bms.config;
 
 import com.splat.bms.model.AdminAction;
 import com.splat.bms.model.Banner;
+import com.splat.bms.model.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
@@ -33,6 +34,18 @@ public class Mapping {
                     .setHeight(resultSet.getInt("height"))
                     .setTargetUrl(resultSet.getString("target_url"))
                     .setLangId(resultSet.getInt("lang_id"));
+        };
+    }
+
+    @Bean
+    RowMapper<User> getUserRowMapper() {
+        return (resultSet, i) -> {
+            return new User()
+                    .setId(resultSet.getInt("id"))
+                    .setUsername(resultSet.getString("login"))
+                    .setPassword(resultSet.getString("password"))
+                    .setCreationDate(new Date(resultSet.getTimestamp("date").getTime()))
+                    .setEnabled(resultSet.getBoolean("is_enabled"));
         };
     }
 
